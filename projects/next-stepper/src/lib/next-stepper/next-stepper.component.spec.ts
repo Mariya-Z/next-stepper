048f1038eb1fa999e9c47f88178e871f58b40921
Mariya-Z <zubkovaa.mariya@yandex.ru>
2019-03-17 20:46:31 +0300

    fix test for app and add test foe lib

--- projects/next-stepper/src/lib/next-stepper/next-stepper.component.spec.ts
+++ projects/next-stepper/src/lib/next-stepper/next-stepper.component.spec.ts
@@ -1,6 +1,8 @@
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {NextStepperComponent} from './next-stepper.component';
+import {Component, NO_ERRORS_SCHEMA} from '@angular/core';
+import {By} from '@angular/platform-browser';
 
 describe('NextStepperComponent', () => {
   let component: NextStepperComponent;
@@ -22,3 +24,54 @@ describe('NextStepperComponent', () => {
     expect(component).toBeTruthy();
   });
 });
+
+describe('NextStepperHostComponent', () => {
+  let component: NextStepperHostComponent;
+  let fixture: ComponentFixture<NextStepperHostComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [NextStepperHostComponent],
+      schemas: [NO_ERRORS_SCHEMA],
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(NextStepperHostComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should show step names', () => {
+    fixture.detectChanges();
+    fixture.whenStable().then(() => {
+      const de = fixture.debugElement.query(By.css('.next-stepper'));
+      console.log(de.nativeElement);
+      expect(de.nativeElement.textContent).toBe('firstStep');
+    });
+  });
+});
+
+@Component({
+  template: `
+    <next-stepper
+      [firstStep]="'firstStep'"
+      [firstChecked]="firstChecked"
+      [firstDisabled]="firstDisabled"
+      [secondStep]="'secondStep'"
+      [secondChecked]="secondChecked"
+      [secondDisabled]="secondDisabled"
+      [thirdStep]="'thirdStep'"
+      [thirdChecked]="thirdChecked"
+      [thirdDisabled]="thirdDisabled"
+    ></next-stepper>
+  `,
+})
+class NextStepperHostComponent {
+  public firstChecked = true;
+  public firstDisabled = false;
+  public secondChecked = false;
+  public secondDisabled = true;
+  public thirdChecked = false;
+  public thirdDisabled = true;
+}
