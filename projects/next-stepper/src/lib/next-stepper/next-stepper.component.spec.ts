3133882b1cd0dffb7cafed285e92a8aefb28f782
Mariya-Z <35897561+Mariya-Z@users.noreply.github.com>
2019-03-18 11:55:23 +0300

    3 unit tests (#7)
    * fix test for app and add test foe lib
    
    * refactoring lib tests
    
    * add scripts

--- projects/next-stepper/src/lib/next-stepper/next-stepper.component.spec.ts
+++ projects/next-stepper/src/lib/next-stepper/next-stepper.component.spec.ts
@@ -1,18 +1,22 @@
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {NextStepperComponent} from './next-stepper.component';
+import {Component} from '@angular/core';
+import {By} from '@angular/platform-browser';
 
 describe('NextStepperComponent', () => {
   let component: NextStepperComponent;
   let fixture: ComponentFixture<NextStepperComponent>;
+  let hostFixture: ComponentFixture<NextStepperHostComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [NextStepperComponent],
+      declarations: [NextStepperComponent, NextStepperHostComponent],
     }).compileComponents();
   }));
 
   beforeEach(() => {
+    hostFixture = TestBed.createComponent(NextStepperHostComponent);
     fixture = TestBed.createComponent(NextStepperComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -21,4 +25,56 @@ describe('NextStepperComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should show step names', () => {
+    hostFixture.detectChanges();
+    hostFixture.whenStable().then(() => {
+      let element = hostFixture.debugElement.query(By.css('.first-step .step-title'));
+      expect(element.nativeElement.textContent).toBe('firstStep');
+
+      element = hostFixture.debugElement.query(By.css('.second-step .step-title'));
+      expect(element.nativeElement.textContent).toBe('secondStep');
+
+      element = hostFixture.debugElement.query(By.css('.third-step .step-title'));
+      expect(element.nativeElement.textContent).toBe('thirdStep');
+    });
+  });
+
+  it('should set necessary classes', () => {
+    hostFixture.detectChanges();
+    hostFixture.whenStable().then(() => {
+      let element = hostFixture.debugElement.query(By.css('.first-step'));
+      expect(element.nativeElement.className).toContain('checked', 'active');
+
+      element = hostFixture.debugElement.query(By.css('.second-step'));
+      expect(element.nativeElement.className).toContain('disabled');
+
+      element = hostFixture.debugElement.query(By.css('.third-step'));
+      expect(element.nativeElement.className).toContain('disabled');
+    });
+  });
 });
+
+@Component({
+  template: `
+    <next-stepper
+      [firstStep]="'firstStep'"
+      [firstChecked]="firstChecked"
+      [firstDisabled]="firstDisabled"
+      [secondStep]="'secondStep'"
+      [secondChecked]="secondChecked"
+      [secondDisabled]="secondDisabled"
+      [thirdStep]="'thirdStep'"
+      [thirdChecked]="thirdChecked"
+      [thirdDisabled]="thirdDisabled"
+    ></next-stepper>
+  `,
+})
+class NextStepperHostComponent {
+  public firstChecked = true;
+  public firstDisabled = false;
+  public secondChecked = false;
+  public secondDisabled = true;
+  public thirdChecked = false;
+  public thirdDisabled = true;
+}
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,12 @@
 import {TestBed, async} from '@angular/core/testing';
 import {AppComponent} from './app.component';
+import {NextStepperModule} from 'next-stepper';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [AppComponent],
+      imports: [NextStepperModule],
     }).compileComponents();
   }));
 
@@ -12,18 +14,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'next-stepper-lib-app'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('next-stepper-lib-app');
-  });
-
-  it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to next-stepper-lib-app!');
   });
 });
