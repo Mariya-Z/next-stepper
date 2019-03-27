5fec5c1dc0e14d73947dfe63242b83bf6fdb6b2a
Mariya-Z <zubkovaa.mariya@yandex.ru>
2019-03-27 09:17:04 +0300

    update inputs

--- projects/next-stepper/src/lib/next-stepper/next-stepper.component.ts
+++ projects/next-stepper/src/lib/next-stepper/next-stepper.component.ts
@@ -1,6 +1,12 @@
 import {Component, Input, OnInit} from '@angular/core';
 // import { ChangeDetectionStrategy } from '@angular/compiler/src/core';
 
+interface Step {
+  id: string;
+  name: string;
+  allowTransition: boolean;
+}
+
 @Component({
   selector: 'next-stepper',
   templateUrl: './next-stepper.component.html',
@@ -8,45 +14,53 @@ import {Component, Input, OnInit} from '@angular/core';
   // changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class NextStepperComponent implements OnInit {
-  @Input() public firstStep: string;
-  @Input() public secondStep: string;
-  @Input() public thirdStep: string;
+  @Input() public steps: Step[] = [];
 
-  public checkedSteps = [true, false, false];
-  public disabledSteps = [false, true, true];
+  public activeSteps = [];
+  public passiveSteps = [];
+  // public checkedSteps = [true, false, false];
+  public disabledSteps;
 
-  public get activeSteps(): boolean[] {
-    this.disabledSteps.forEach((v, i) => (this.activeSteps[i] = !this.disabledSteps[i]));
-    return this.activeSteps;
-  }
+
+  // public get activeSteps(): boolean[] {
+  //   this.disabledSteps.forEach((v, i) => (this.activeSteps[i] = !this.disabledSteps[i]));
+  //   return this.activeSteps;
+  // }
 
   @Input() private checked: number;
 
   public ngOnInit(): void {
+    // console.log(this.steps);
+    // console.log(this.checkedSteps);
+    this.activeSteps = this.steps.map(() => false);
+    this.disabledSteps = this.steps.forEach((item) => !item.allowTransition);
     if (this.checked >= 0) {
-      for (let i = 0; i <= this.checked; ++i) {
-        this.disabledSteps[i] = false;
-      }
-      this.checkedSteps[0] = false;
-      this.checkedSteps[this.checked] = true;
+      this.activeSteps[this.checked] = true;
+      // for (let i = 0; i <= this.checked; ++i) {
+      //   this.disabledSteps[i] = false;
+      // }
+      this.activeSteps[0] = false;
+      this.activeSteps[this.checked] = true;
+    } else {
+      this.activeSteps[0] = true;
     }
   }
 
   public onClick(i: number): void {
-    if (this.disabledSteps[i] === false) {
-      this.checkedSteps.forEach((v, j) => {
-        this.checkedSteps[j] = false;
-      });
-      this.checkedSteps[i] = true;
-    }
+    // if (this.disabledSteps[i] === false) {
+    //   this.checkedSteps.forEach((v, j) => {
+    //     this.checkedSteps[j] = false;
+    //   });
+    //   this.checkedSteps[i] = true;
+    // }
   }
 
-  public next(): void {
-    const i = this.checkedSteps.indexOf(true);
-    if (i < 2) {
-      this.checkedSteps[i] = false;
-      this.checkedSteps[i + 1] = true;
-      this.disabledSteps[i + 1] = false;
-    }
-  }
+  // public next(): void {
+  //   const i = this.checkedSteps.indexOf(true);
+  //   if (i < 2) {
+  //     this.checkedSteps[i] = false;
+  //     this.checkedSteps[i + 1] = true;
+  //     this.disabledSteps[i + 1] = false;
+  //   }
+  // }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,17 @@ import { NextStepperComponent } from 'next-stepper';
 })
 export class AppComponent {
   public checked = 1;
+
+  public steps = [
+    {id: '1', name: 'firstStep', allowTransition: true},
+    {id: '3', name: 'secondStep', allowTransition: true},
+    {id: '2', name: 'thirdStep', allowTransition: true},
+    {id: '4', name: 'fouthStep', allowTransition: true},
+  ];
+
   @ViewChild(NextStepperComponent) public stepper: NextStepperComponent;
 
   public onClick(): void {
-    this.stepper.next();
+    // this.stepper.next();
   }
 }
